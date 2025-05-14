5582beb7fa8f42fad80105147a65f228c2ebc94c
midnight <83584402+midatindex0@users.noreply.github.com>
2025-05-14 10:41:31 +0530

    add td_write to async.h

--- include/tandem/async.h
+++ include/tandem/async.h
@@ -5,7 +5,9 @@
 
 void td_sch_init(td_rt *rt);
 void td_sch_loop(td_rt *rt);
+
 void td_sleep(td_rt *rt, size_t millis);
 ssize_t td_read(td_rt *rt, int fd, void *buf, size_t count);
+ssize_t td_write(td_rt *rt, int fd, void *buf, size_t count);
 
 #endif
